75d1ffd4928c4e82b6bca4e51c1b78673f485531
DiegodelaCruz12 <86082852+DiegodelaCruz12@users.noreply.github.com>
2022-05-26 01:35:15 -0500

    3
    3

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express= require('express');
 const morgan=require('morgan');
-//const Handlebars=require('Handlebars');
+const Handlebars=require('Handlebars');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
